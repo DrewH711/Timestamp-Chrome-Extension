d649da381fc3fc8928960b50ab66735fbd5062ee
Drew Holland <83188352+DrewH711@users.noreply.github.com>
2026-06-21 11:39:26 -0400

    Refactor date and message variable declarations

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const date = new Date();
 
-var month = date.getMonth();
-var day = date.getDate();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var initials = "No initials entered";
-
-var message = `${hours}:${minutes} ${date.getMonth()}/${date.getDate()} - ${initials} `
-
 document.getElementById("submit_button").addEventListener("click", () => {
-  initials = document.getElementById("initials").value;
+  let month = date.getMonth();
+  let day = date.getDate();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let initials = document.getElementById("initials").value;
+
+  const message = `${hours}:${minutes} ${date.getMonth()}/${date.getDate()} - ${initials} `
   alert(message);
 });
 
