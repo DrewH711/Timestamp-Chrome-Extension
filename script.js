0d1ef79e8cd168e54a8096aad271fb87c75aa476
Drew Holland <83188352+DrewH711@users.noreply.github.com>
2026-06-21 11:19:54 -0400

    Modify message to display time and date
    Updated message format to include time and date.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const date = new Date();
 
+var month = date.getMonth();
+var day = date.getDate();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+
+let message = `${hours}:${minutes} ${date.getMonth()}/${date.getDate()} - ${initials} `
+
 document.getElementById("initials").addEventListener("click", () => {
-alert("Button clicked!");
+alert(message);
 });
 
-let message = `${initials} ${date.getMonth()}/${date.getDate()}`
+
