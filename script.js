9e3b0b31a1d693ac02c1980c20c5d7705c99a4d1
Drew Holland <83188352+DrewH711@users.noreply.github.com>
2026-06-21 12:10:33 -0400

    Implement getSavedInitials function
    Added function to retrieve saved initials from cookies.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const date = new Date();
 
+function getSavedInitials(){
+  let decodedCookie = decodeURIComponent(document.cookie);
+  if(decodedCookie.length>9){
+    //initials= is 9 characters
+    let value = decodedCookie.substring(9, decodedCookie.length);
+    return value;
+  }
+  else{
+    return "";
+  }
+}
+
 document.getElementById("submit_button").addEventListener("click", () => {
   let month = date.getMonth();
   let day = date.getDate();
@@ -7,7 +19,11 @@ document.getElementById("submit_button").addEventListener("click", () => {
   let minutes = date.getMinutes();
   let initials = document.getElementById("initials").value;
 
-  const message = `${hours}:${minutes} ${date.getMonth()}/${date.getDate()} - ${initials} `
+  if(getSavedInitials() != initials){
+    document.cookie = `initials=${initials}`;
+  }
+
+  var message = `${hours}:${minutes} ${date.getMonth()}/${date.getDate()} - ${getSavedInitials()}`;
   alert(message);
 });
 
