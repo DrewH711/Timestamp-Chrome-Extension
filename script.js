c8255989e83ed5a277002d526a11c2f4b2eea216
Drew Holland <drexh47127@gmail.com>
2026-06-22 19:16:57 -0400

    Working but only inside browser extension menu

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
 const date = new Date();
 
-function getSavedInitials(){
-  let decodedCookie = decodeURIComponent(document.cookie);
-  if(decodedCookie.length>9){
-    //initials= is 9 characters
-    let value = decodedCookie.substring(9, decodedCookie.length);
-    return value;
-  }
-  else{
-    return "";
-  }
+document.getElementById("submit_button").value = "Save";
+
+try{
+  document.getElementById("initials").value = localStorage.getItem("initials");
+}
+catch(error){
+  localStorage.setItem("initials", "");
 }
 
 document.getElementById("submit_button").addEventListener("click", () => {
-  let month = date.getMonth();
-  let day = date.getDate();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let initials = document.getElementById("initials").value;
+  let newinitials = document.getElementById("initials").value;
+  localStorage.setItem("initials", newinitials);
+  document.getElementById("submit_button").value = "Initials saved!";
+});
 
-  if(getSavedInitials() != initials && gtSavedInitials()!=""){
-    document.cookie = `initials=${initials}`;
+function insertAtCursor(element, text) {
+  const start = element.selectionStart;
+  const end = element.selectionEnd;
+  element.value = element.value.slice(0, start) + text + element.value.slice(end);
+  element.selectionStart = element.selectionEnd = start + text.length;
+  element.focus();
+}
+
+window.addEventListener("keydown", function (event) {
+
+  if(event.ctrlKey && event.shiftKey && event.key==="F"){
+
+    let month = date.getMonth();
+    let day = date.getDate();
+    let hours = date.getHours();
+    let amOrPm = "am";
+
+    if(hours>12){
+      hours = hours % 12;
+      amOrPm = "pm";
+    }
+    
+    let minutes = date.getMinutes();
+    let dateTimeInitials = `${hours}:${minutes}${amOrPm} ${month}/${day} - ${localStorage.getItem("initials")}`;
+    // alert(dateTimeInitials);
+    try{
+      insertAtCursor(this.window.document.activeElement, dateTimeInitials);
+    }
+    catch(error){
+      alert("An error occurred: " + error)
+    }
   }
 
-  var message = `${hours}:${minutes} ${date.getMonth()}/${date.getDate()} - ${getSavedInitials()}`;
-  alert(message);
 });
 
 
